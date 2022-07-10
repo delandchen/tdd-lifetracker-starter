cf3dd0b105c1d421d18bb25109bc64b3bb3bd72d
unknown <chen.deland@gmail.com>
2022-07-09 23:02:49 -0400

    Deploying API

--- lifetracker-api/config.js
+++ lifetracker-api/config.js
@@ -4,6 +4,7 @@ require('colors');
 const PORT = process.env.PORT ? Number(process.env.PORT) : 3001;
 const BCRYPT_WORK_FACTOR = process.env.BCRYPT_WORK_FACTOR ? Number(process.env.BCRYPT_WORK_FACTOR) : 13;
 const SECRET_KEY = process.env.SECRET_KEY || "123secretkey123";
+const REACT_APP_REMOTE_HOST_URL = process.env.REACT_APP_REMOTE_HOST_URL;
 
 const getDatabaseUri = () => {
     const dbUser = process.env.DATABASE_USER || "postgres"
@@ -25,4 +26,5 @@ module.exports = {
     getDatabaseUri,
     BCRYPT_WORK_FACTOR,
     SECRET_KEY,
+    REACT_APP_REMOTE_HOST_URL,
 }
